7d6d2e7625f228e985aaf338b0b649db8276d7f4
denisfelikson <>
2018-10-09 09:44:41 -0400

    Fixed typo in matfile2tif.py.

--- pythonModules/matfile2tif.py
+++ pythonModules/matfile2tif.py
@@ -2,19 +2,30 @@
 
 import sys
 sys.path.append('/Users/dfelikso/Research/Software/ScriptsAndUtilities/pythonModules/')
-
 import raster
-
 from scipy.io import loadmat
 
-m = loadmat('GrIS_Tidewater_basins.mat')
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument('matfile', type=str, help='matfile filename')
+parser.add_argument('-x',      type=str, help='name of variable with x coordinates', default='x')
+parser.add_argument('-y',      type=str, help='name of variable with y coordinates', default='y')
+parser.add_argument('-z',      type=str, help='name of variable with z coordinates', default='z')
 
-x_upperleft = m['x'][0][0]
-y_upperleft = m['y'][0][0]
-x_step = m['x'][0][1] - m['x'][0][0]
-y_step = m['y'][0][1] - m['y'][0][0]
+args = parser.parse_args()
+matfile = args.matfile
+x_var = args.x
+y_var = args.y
+z_var = args.z
+
+m = loadmat(matfile)
+
+x_upperleft = m[x_var][0][0]
+y_upperleft = m[y_var][0][0]
+x_step = m[x_var][0][1] - m[x_var][0][0]
+y_step = m[y_var][0][1] - m[y_var][0][0]
 
 gt = (x_upperleft, x_step, 0., y_upperleft, 0., y_step)
-raster.writeArrayAsRasterBand('GrIS_Tidewater_basins.tif', gt, m['tidewater_basins'], -9999.)
+raster.writeArrayAsRasterBand(matfile+'.tif', gt, m[z_var], -9999.)
 
