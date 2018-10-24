cfddfbe1a0c2352aaa954fd79c93d0d0dfd028fa
denisfelikson <>
2018-10-24 09:37:36 -0400

    Added to shapefile_utils.py the ability to pass along attributes and values to buffered shapefile.

--- pythonModules/shapefile_utils.py
+++ pythonModules/shapefile_utils.py
@@ -1,6 +1,36 @@
 
+import os
 import ogr
 import numpy as np
+
+def createBuffer(inputfn, outputBufferfn, bufferDist):
+   inputds = ogr.Open(inputfn)
+   inputlyr = inputds.GetLayer()
+
+   shpdriver = ogr.GetDriverByName('ESRI Shapefile')
+   if os.path.exists(outputBufferfn):
+      shpdriver.DeleteDataSource(outputBufferfn)
+   outputBufferds = shpdriver.CreateDataSource(outputBufferfn)
+   
+   bufferlyr = outputBufferds.CreateLayer(outputBufferfn, geom_type=ogr.wkbPolygon)
+   featureDefn = bufferlyr.GetLayerDefn()
+   
+   inputlyrDefn = inputlyr.GetLayerDefn()
+   for i in range(inputlyrDefn.GetFieldCount()):
+      bufferlyr.CreateField ( inputlyrDefn.GetFieldDefn(i) )
+    
+   for feature in inputlyr:
+      ingeom = feature.GetGeometryRef()
+      geomBuffer = ingeom.Buffer(bufferDist)
+      
+      outFeature = ogr.Feature(featureDefn)
+      outFeature.SetGeometry(geomBuffer)
+      
+      for i in range(inputlyrDefn.GetFieldCount()): 
+         fieldName = inputlyrDefn.GetFieldDefn(i).GetName()
+         outFeature.SetField(fieldName, feature.GetField(fieldName))
+      bufferlyr.CreateFeature(outFeature)
+      outFeature = None
 
 def read_shapefile_points(shapefile):
    if shapefile.endswith('.shp'):
@@ -13,7 +43,7 @@ def read_shapefile_points(shapefile):
       y = np.array([])
       points = geom.GetPoints()
       if points is None:
-         print 'ERROR: no points found in shapefile'
+         print('ERROR: no points found in shapefile')
          return None, None
 
       for i, pointxy in enumerate(points):
@@ -28,3 +58,4 @@ def read_shapefile_points(shapefile):
       y = data[:,ycol]
      
    return x, y
+
